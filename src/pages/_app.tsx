0256ae1979fd8a5d73ab3ea870c7a90efdd74243
komekami <komekami0131@gmail.com>
2022-10-07 07:23:09 +0900

    feat: Next.jsアプリの環境設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { createGlobalStyle } from "styled-components";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 html,
@@ -26,24 +26,24 @@ a {
 ol, ul {
   list-style: none;
 }
-`;
+`
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta key="charset" name="charset" content="utf-8" />
+        <meta key="charset" name='charset' content='utf-8' />
         <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+          key='viewport'
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5'
         />
-        <meta property="og:locale" content="ja_JP" />
-        <meta property="og:type" content="website" />
+        <meta property='og:locale' content='ja_JP' />
+        <meta property='og:type' content='website' />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
---
+++ src/themes/lineHeights.ts
@@ -0,0 +1,11 @@
+const letterHeights: string[] = [
+  '17px',
+  '19px',
+  '22px',
+  '26px',
+  '28px',
+  '37px',
+  '43px',
+]
+
+export default letterHeights
